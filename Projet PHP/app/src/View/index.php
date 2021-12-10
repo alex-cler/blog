76393c0d7b470cf85a6a7643c29cb23f2b811580
Alex <alex28.cler@gmail.com>
2021-12-10 15:28:54 +0100

    Edit : Bootstrap ...

--- Projet PHP/app/src/View/index.php
+++ Projet PHP/app/src/View/index.php
@@ -1,3 +1,5 @@
+<?php
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,7 +10,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
           rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
           crossorigin="anonymous">
-    <link rel="stylesheet" href="./style/bootstrap.css">
+    <link rel="stylesheet" href="style/bootstrap.css">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
@@ -21,15 +23,13 @@
         <div class="collapse navbar-collapse" id="navbarColor01">
             <ul class="navbar-nav me-auto">
                 <li class="nav-item">
-                    <a class="nav-link active" href="#">Home
-                        <span class="visually-hidden">(current)</span>
-                    </a>
+                    <a class="nav-link active" href="#">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Paramètre</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="./Frontend/post.php">Post</a>
+                    <a class="nav-link" href="Frontend/post.php">Post</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="./Frontend/admin.php">A propos</a>
